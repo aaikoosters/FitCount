23c5e6da4275b649c56e7c8a64849de70c1194a8
Aaik Oosters <aaik.oosters@gmail.com>
2023-01-23 15:34:27 +0100

    Initial Commit

---
+++ FitCount/FitCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitCountApp.swift
+//  FitCount
+//
+//  Created by Aaik Oosters on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
